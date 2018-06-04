1aa1f613fb7a57441233f2b33552e851c2fff0c7
YanS0ng <c.yansong@columbia.edu>
2018-06-04 18:44:38 -0400

    test: Define cache table

---
+++ Simple-Chat/include/simple_chat.h
@@ -0,0 +1,26 @@
+/*******************************************************************************
+* simple_chat.h
+* 
+* Defines structs and classes for the simple chat application.
+*
+* Author: Yan-Song Chen
+* Date  : Jun 4th, 2018
+*******************************************************************************/
+#include <unordered_map>
+#include <string>
+#include <iostream>
+
+typedef struct {
+  unsigned long ip;
+  unsigned short port;
+  bool on;
+} table_row_t;
+
+std::ostream& operator<<(std::ostream& os, const table_row_t& r) {
+  os << r.ip << ':' << r.port;
+  if (r.on) os << " (on)";
+  else os << " (off)";
+  return os;
+}
+
+typedef std::unordered_map<std::string,table_row_t> table_t;
