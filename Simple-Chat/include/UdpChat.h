7a7109e090cf786b07b4c5d04e0c27b09c488ca7
YanS0ng <c.yansong@columbia.edu>
2018-06-06 22:38:35 -0400

    feat: The UdpSocket is usable now.
    Follow-up:
    1. Define my application level protocol.
    2. Store table_t in main program and maintain its values.

--- Simple-Chat/include/UdpChat.h
+++ Simple-Chat/include/UdpChat.h
@@ -4,7 +4,7 @@
 * Defines structs and classes for the simple chat application.
 *
 * Author: Yan-Song Chen
-* Date  : Jun 4th, 2018
+* Date  : Jun 6th, 2018
 *******************************************************************************/
 #include <unordered_map>
 #include <string>
@@ -14,11 +14,15 @@
 #include <unistd.h>
 #include <cstring>
 
+
+// Table struct that stores client information
 typedef struct {
   unsigned long ip;
   unsigned short port;
   bool on;
 } table_row_t;
+
+typedef std::unordered_map<std::string,table_row_t> table_t;
 
 std::ostream& operator<<(std::ostream& os, const table_row_t& r) {
   os << r.ip << ':' << r.port;
@@ -27,17 +31,31 @@ std::ostream& operator<<(std::ostream& os, const table_row_t& r) {
   return os;
 }
 
-typedef std::unordered_map<std::string,table_row_t> table_t;
-
 std::ostream& operator<<(std::ostream& os, const table_t& t) {
   for (const std::pair<std::string,table_row_t>& r : t)
     os << r.first << ' ' << r.second << std::endl;
   return os;
 }
 
+
+// Udp message type that will be exported by the socket wrapper. The ip and port
+// number are converted to standard types when parsed to the main program.
+typedef struct {
+  std::string ip;
+  uint16_t port;
+  std::string msg;
+} udpmsg_t;
+
+
+// Udp socket wrapper
+// Encapsulates C-style system calls and allows the main program to focus on
+// higher-level tasks (e.g. protocal exchange).
 class UdpSocket {
 public:
-  UdpSocket(unsigned short port) {
+  UdpSocket(uint16_t port) {
+    if ((fd_ = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+      throw "error: cannot create socket";
+    }
     memset(&myaddr_, 0, sizeof(myaddr_));
     myaddr_.sin_family = AF_INET;
     myaddr_.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -46,30 +64,41 @@ public:
     if (bind(fd_, (struct sockaddr*)&myaddr_, sizeof(myaddr_))<0) {
       throw "error: bind failed";
     }
+    std::cout << "Register at " << inet_ntoa(myaddr_.sin_addr) << ':';
+    std::cout << ntohs(myaddr_.sin_port) << std::endl;
   }
 
-  void SetServer(char* servip, unsigned short servport,
-                 unsigned short clntport) {
-    memset((char*)&servaddr_, 0, sizeof(servaddr_));
-    servaddr_.sin_family = AF_INET;
-    servaddr_.sin_port = htons(servport);
-    if (inet_aton(servip,&servaddr_.sin_addr) < 0) {
-      throw "error: invalid server ip";
+  ~UdpSocket() {
+    close(fd_);
+  }
+
+  void SendTo(char* ip, uint16_t port, const std::string& msg) {
+    memset((char*)&clntaddr_, 0, sizeof(clntaddr_));
+    clntaddr_.sin_family = AF_INET;
+    clntaddr_.sin_port = htons(port);
+    if (inet_aton(ip, &clntaddr_.sin_addr) < 0) {
+      throw "error: invalid ip";
     };
+    if (sendto(fd_, msg.c_str(), msg.size(), 0, (struct sockaddr *)&clntaddr_,
+        sizeof(clntaddr_)) < 0) {
+      throw "error: sendto failed";
+    }
   }
 
-  std::string Listen() {
+  udpmsg_t Listen() {
       size_t recvlen = recvfrom(fd_, buf_, BUFSIZE, 0,
                                 (struct sockaddr*)&clntaddr_, &addrlen_);
-      return std::string((char*) buf_, recvlen);
+      buf_[recvlen] = 0;
+      return udpmsg_t{std::string(inet_ntoa(clntaddr_.sin_addr)),
+                      ntohs(clntaddr_.sin_port),
+                      std::string((char*) buf_, recvlen)};
   }
+
 private:
   static const size_t BUFSIZE = 2048;
   int fd_;
   struct sockaddr_in myaddr_;
-  struct sockaddr_in servaddr_; // server address, optional
-  struct sockaddr_in clntaddr_; // stores sender's address
+  struct sockaddr_in clntaddr_; // Target addr (talker), sender addr (listener)
   socklen_t addrlen_ = sizeof(clntaddr_);
   unsigned char buf_[BUFSIZE];
-  table_t table_;
 };
--- Simple-Chat/src/UdpChat.cc
+++ Simple-Chat/src/UdpChat.cc
@@ -4,118 +4,59 @@
 * Simple chat application.
 *
 * Author: Yan-Song Chen
-* Date  : Jun 5th, 2018
+* Date  : Jun 6th, 2018
 *******************************************************************************/
 #include <UdpChat.h>
 
 const size_t BUFSIZE = 2048;
 int main(int argc, char** argv) {
+  /*****************************************************************************
+  *  Server code starts from here
+  *****************************************************************************/
   if (std::string(argv[1]) == "-s") {
     if (argc < 2) {
       throw "error: too few arguments";
     }
-    /**************************************************************************
-    *  Server code starts from here
-    ***************************************************************************/
-    unsigned short port = strtoul(argv[2], nullptr, 0);
-    std::cout << "Server mode at port " << port << std::endl;
-
-    struct sockaddr_in servaddr;
-    struct sockaddr_in clntaddr;
-    socklen_t addrlen = sizeof(clntaddr);
-    int recvlen;
-    int fd;
-    unsigned char buf[BUFSIZE];
-
-    // Create a Udp socket
-    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-      throw "error: cannot create socket";
-    }
-    // Bind the socket
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servaddr.sin_port = htons(port);
-
-    if (bind(fd, (struct sockaddr*)&servaddr, sizeof(servaddr))<0) {
-      throw "error: bind failed";
-    }
+    uint16_t port = strtoul(argv[2], nullptr, 0);
+    UdpSocket udp(port);
+    std::cout << "Server mode" << std::endl;
 
     while (true) {
-      recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr*)&clntaddr,
-                         &addrlen);
-      if (recvlen > 0) {
-        buf[recvlen] = 0;
-	std::cout << buf << std::endl;
-      }
-      if (sendto(fd, buf, recvlen, 0, (struct sockaddr *)&clntaddr,
-          sizeof(clntaddr)) < 0) {
-        throw "error: sendto failed";
-      }
+      udpmsg_t msg = udp.Listen();
+      std::cout << "Receive msg from " << msg.ip << ':';
+      std::cout << msg.port << std::endl;
+      std::cout << "Content " << msg.msg << std::endl;
     }
 
-    close(fd);
-
+  /*****************************************************************************
+  *  Client code starts from here
+  *****************************************************************************/
   } else if (std::string(argv[1]) == "-c") {
     if (argc < 5) {
       throw "error: too few arguments";
     }
-    /**************************************************************************
-    *  Client code starts from here
-    ***************************************************************************/
-    struct sockaddr_in myaddr;
-    struct sockaddr_in clntaddr;
-    socklen_t addrlen = sizeof(clntaddr);
-    int recvlen;
-    int fd;
-    unsigned char buf[BUFSIZE];
-    // Create server Udp socket
-    if ((fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-      throw "error: cannot create socket";
-    }
-    // Bind the socket
-    memset(&myaddr, 0, sizeof(myaddr));
-    myaddr.sin_family = AF_INET;
-    myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    unsigned short myport = strtoul(argv[4], nullptr, 0);
-    myaddr.sin_port = htons(myport);
-
-    if (bind(fd, (struct sockaddr*)&myaddr, sizeof(myaddr))<0) {
-      throw "error: bind failed";
-    }
+    uint16_t myport = strtoul(argv[4], nullptr, 0);
+    UdpSocket udp(myport);
+    std::cout << "Client mode" << std::endl;
 
     pid_t pid = fork();
 
-    if (pid == 0) {
+    // Listener: response to a udp message
+    if (pid > 0) {
       while (true) {
-        recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr*)&clntaddr,
-                           &addrlen);
-        if (recvlen > 0) {
-          buf[recvlen] = 0;
-          std::cout << "echo: " << buf << std::endl << ">>> " << std::flush;
-        }
+        udpmsg_t msg = udp.Listen();
+        std::cout << "echo: " << msg.msg << std::endl << ">>> " << std::flush;
       }
+    // Talker: take input from the user
     } else {
-      struct sockaddr_in servaddr;
-      memset((char*)&servaddr, 0, sizeof(servaddr));
-      servaddr.sin_family = AF_INET;
-      unsigned short servport = strtoul(argv[3], nullptr, 0);
-      servaddr.sin_port = htons(servport);
-      if (inet_aton(argv[2],&servaddr.sin_addr) < 0) {
-        throw "error: invalid server ip";
-      };
-
+      uint16_t servport = strtoul(argv[3], nullptr, 0);
       while (true) {
         std::string msg;
         std::cout << ">>> " << std::flush;
         getline(std::cin, msg);
-        if (sendto(fd, msg.c_str(), msg.size(), 0, (struct sockaddr *)&servaddr,
-	    sizeof(servaddr)) < 0) {
-          throw "error: sendto failed";
-        }
+	udp.SendTo(argv[2], servport, msg);
       }
     }
-    close(fd);
   } else {
     std::cout << "warning: unknown argument " << argv[1] << std::endl;
   }
