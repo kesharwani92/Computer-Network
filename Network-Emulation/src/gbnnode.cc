b3458b0d9544cecfbada30e9de66166dca5f4206
YanS0ng <c.yansong@columbia.edu>
2018-06-23 23:26:46 -0400

    feat: Receiver side works correctly
    The code is truly messy though...

--- Network-Emulation/src/gbnnode.cc
+++ Network-Emulation/src/gbnnode.cc
@@ -6,7 +6,7 @@
 #include <string>
 #include <queue>
 
-#define GBNNODE_DEBUG 1 // Debugging mode
+#define GBNNODE_DEBUG 0 // Debugging mode
 ////////////////////////////////////////////////////////////////////////////////
 // Move to gbnnode.h
 ////////////////////////////////////////////////////////////////////////////////
@@ -19,6 +19,8 @@ std::queue<std::string> messages;
 
 std::atomic_flag acklock = ATOMIC_FLAG_INIT; // lock for ACKs
 size_t ack;
+
+
 
 static int fd;
 static struct sockaddr_in src, dest;
@@ -52,7 +54,7 @@ void cin_proc() {
     size_t delim = line.find(' ');
     if (line.substr(0, delim) != "send") {
       #if GBNNODE_DEBUG
-      MY_ERROR_STREAM << "warning: support only send operation" << std::endl;
+      std::cout << "warning: support only send operation" << std::endl;
       #endif
       continue;
     }
@@ -60,7 +62,7 @@ void cin_proc() {
     std::string message = line.substr(delim+1, line.size()-delim);
 
     #if GBNNODE_DEBUG
-    MY_INFO_STREAM << "cin_proc: got " << message << std::endl;
+    std::cout << "cin_proc: got " << message << std::endl;
     #endif
     __grab_lock(msglock);
     messages.push(message);
@@ -81,7 +83,6 @@ bool drop_packet() {
 }
 
 // The go-back-n protocol thread
-static jmp_buf env;
 void __timeout_handler(int signo);
 void __gbn_fsm(const std::string& buf, const int& window);
 // Pops out the top message and send it over UDP
@@ -91,7 +92,7 @@ void gbn_proc(const int& window) {
     __grab_lock(msglock);
     std::string buf = messages.front();
     #if GBNNODE_DEBUG
-    MY_INFO_STREAM << "gbn_proc: send " << buf << std::endl;
+    std::cout << "gbn_proc: send " << buf << std::endl;
     #endif
     messages.pop();
     __release_lock(msglock);
@@ -103,67 +104,86 @@ void gbn_proc(const int& window) {
   }
 }
 
+bool __timeout_flag;
 void __timeout_handler(int signo) {
-  longjmp(env, ETIME); // timeout errno
+  MY_INFO_STREAM << "timeout" << std::endl;
+  //longjmp(env, ETIME); // timeout errno
+  __timeout_flag = true;
 }
 
 // Implementation of GBN Finite State Machine
 void __gbn_fsm(const std::string& buf, const int& window) {
   size_t base = 0, nextseq = 0;
-  //for (nextseq = 0; nextseq < buf.size(); nextseq++) {
-  //  std::string msg = "SEQ:" + std::to_string(nextseq) + ":" + buf[nextseq];
-  //  __send(msg);
-  //}
   struct sigaction sa;
   sa.sa_handler = &__timeout_handler;
   sigaction (SIGALRM, &sa, 0);
-  
+  __timeout_flag = false;
   ualarm(500000, 0);
-  int val = setjmp(env);
+  //int val = setjmp(env);
   // NOTE: longjmp cannot parse val 0 to setjmp. 0 is for the first run.
-  if (val == 0 || val == 1) {
-    for (; nextseq < base + window && nextseq < buf.size(); nextseq++) {
-      std::string msg = "SEQ:" + std::to_string(nextseq) + ":" + buf[nextseq];
-      __send(msg);
-    }
-
-check_ack:
-    __grab_lock(acklock);
-    base = ack + 1;
-    if (base == buf.size()) {
-      __release_lock(acklock);
-      ualarm(0, 0); // on Linux, setting zero will cancel the alarm
-      return;
-    } else if (base == nextseq) {
-      __release_lock(acklock);
-      ualarm(500000, 0);
-      longjmp(env, 0);
-    }
-    __release_lock(acklock);
-    goto check_ack;
-  } else {
-    for (size_t i = base; i < nextseq; i++) {
-      std::string msg = std::to_string(nextseq) + ":" + buf[nextseq];
-      __send(msg);
-    }
-    ualarm(500000, 0);
-    longjmp(env, 1);
+send_state:
+  if (__timeout_flag) goto timeout_state;
+  for (; nextseq < base + window -1 && nextseq < buf.size(); nextseq++) {
+    std::string msg = "SEQ:" + std::to_string(nextseq) + ":" + buf[nextseq];
+    __send(msg);
+    MY_INFO_STREAM << "packet" << nextseq << ' ' << buf[nextseq] << " sent"
+        << std::endl;
   }
+chkack_state:
+  __grab_lock(acklock);
+  base = ack + 1;
+  if (base == buf.size()) {
+    __release_lock(acklock);
+    ualarm(0, 0); // on Linux, setting zero will cancel the alarm
+    return;
+  } else if (base == nextseq) {
+    __release_lock(acklock);
+    ualarm(500000, 0);
+    //longjmp(env, 0);
+    goto send_state;
+  }
+  __release_lock(acklock);
+  goto send_state;
+timeout_state:
   #if GBNNODE_DEBUG
-  MY_ERROR_STREAM << "__gbn_fsm: something went wrong... shouldn't be here"
-      << std::endl;
-  ualarm(0, 0); 
+  std::cout << "resend " << base << " to " << nextseq << std::endl;
   #endif
+  for (size_t i = base; i < nextseq && i < buf.size(); i++) {
+    std::string msg = "SEQ:" + std::to_string(i) + ":" + buf[i];
+    __send(msg);
+    MY_INFO_STREAM << "packet" << i << ' ' << buf[i] << " sent" << std::endl;
+    #if GBNNODE_DEBUG
+    std::cout << "resend " << msg << std::endl;
+    #endif
+  }
+  ualarm(500000, 0);
+  __timeout_flag = false;
+  goto chkack_state;
+
+  #if GBNNODE_DEBUG
+  std::cout << "__gbn_fsm: something went wrong... shouldn't be here"
+      << std::endl;
+  #endif
+  ualarm(0, 0); 
+  proc_running = false;
 }
 
 // The main thread listens to the bound port
 void recv_proc() {
+  int expected = 0;
   struct sockaddr_in rcvaddr;
   socklen_t addrlen = sizeof(rcvaddr);
   char buf[BUFSIZE];
   while (proc_running) {
-    ssize_t ret = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr*)&rcvaddr,
-        &addrlen);
+    //ssize_t ret = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr*)&rcvaddr,
+    //    &addrlen);
+    ssize_t ret;
+    while ((ret = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr*)&rcvaddr,
+            &addrlen)) == -1 && errno == EINTR);
+    //if (errno == EINTR) {
+    //  std::cout << "ignore alarm siganl, call recvfrom again." << std::endl;
+    //  continue;
+    //}
     if (ret > 0) {
       buf[ret] = 0;
       #if GBNNODE_DEBUG
@@ -183,19 +203,36 @@ void recv_proc() {
       } else if (inmsg.substr(0, pos0) == "SEQ"){
         size_t pos1 = inmsg.find(':', pos0+1);
         int seq = stoi(inmsg.substr(pos0+1, pos1-pos0-1));
-        MY_INFO_STREAM << "packet " << seq << " received" << std::endl;
-        std::string outmsg = "ACK:" + std::to_string(seq);
-        __send(outmsg);
-        MY_INFO_STREAM << "ACK" << seq << " sent, expecting " << seq+1
-            << std::endl;
+        if (drop_packet()) {
+          MY_INFO_STREAM << "packet" << seq << ' ' << inmsg[pos1+1]
+              << " discarded" << std::endl;
+        } else if (seq == expected) {
+          
+          MY_INFO_STREAM << "packet " << seq << ' ' << inmsg[pos1+1]
+              << " received" << std::endl;
+          std::string outmsg = "ACK:" + std::to_string(expected);
+          __send(outmsg);
+          MY_INFO_STREAM << "ACK" << expected << " sent, expecting " 
+              << expected+1 << std::endl;
+          expected++;
+        } else {
+          MY_INFO_STREAM << "packet " << seq << ' ' << inmsg[pos1+1]
+              << " received" << std::endl;
+          std::string outmsg = "ACK:" + std::to_string(expected-1);
+          __send(outmsg);
+          MY_INFO_STREAM << "ACK" << expected-1 << " sent, expecting "
+              << expected << std::endl;
+        }
+
       } else if (inmsg == "END") {
+        expected = 0;
         std::cout << "[Summary] " << "packets discarded, loss rate = ??"
             << std::endl;
       }
     } else {
       std::cerr << "error: recvfrom failed" << std::endl;
-      std::cerr << "error: " <<  strerror(errno);
-      throw std::exception();
+      std::cerr << "error: " <<  strerror(errno) << std::endl;
+      //throw std::exception();
     }
   }
 }
